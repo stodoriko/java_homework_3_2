a7134989612b6e28c46a201b384f34129ab04a8a
stodoriko <s.todorikos@gmail.com>
2021-07-28 00:43:59 +0300

    RubleForMiles

---
+++ src/RubleForMiles.java
@@ -0,0 +1,18 @@
+public class RubleForMiles {
+    public static void main(String[] args) {
+
+        int clientAccount = 100;
+        int refilAmount = 1100;
+        int bonus;
+
+        if (refilAmount >= 1000) {
+            bonus = refilAmount / 100;
+            clientAccount = refilAmount + clientAccount + bonus;
+        } else {
+            clientAccount = refilAmount + clientAccount;
+        }
+
+        System.out.println(clientAccount);
+
+    }
+}
