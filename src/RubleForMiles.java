a8703c6557ea36d6b3f4aa82a350dd5b2286acdd
sergeytodoriko <s.todorikos@gmail.com>
2021-07-29 00:24:00 +0300

    UPD "updated RublesForMiles"

--- src/RubleForMiles.java
+++ src/RubleForMiles.java
@@ -2,10 +2,17 @@ public class RubleForMiles {
     public static void main(String[] args) {
 
         int clientAccount = 100;
-        int refilAmount = 1100;
+        int refilAmount = 1099;
         int bonus;
+        boolean moreThanThousand;
 
         if (refilAmount > 1000) {
+            moreThanThousand = true;
+        } else {
+            moreThanThousand = false;
+        }
+
+        if (moreThanThousand) {
             bonus = refilAmount / 100;
             clientAccount = refilAmount + clientAccount + bonus;
         } else {
